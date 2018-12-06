27ed7c6fcb54e45d097cc15267aaf5c6d1129f69
luisrxo <luis.roberto.diaz.nacar@gmail.com>
2018-12-05 22:42:00 -0600

    Add files via upload

--- prueba.asm
+++ prueba.asm
@@ -3,9 +3,17 @@ section constantes
 	cst2 340
 	cst3 abc
 section codigo
-	mov r1,cst1
+	mov r01,cst1
+	mov r02,r03
+	add var1,r02
+	sub var2,var3
+	mul r05,var1
+	div cst1,cst2
+	mov cst3,r01
 	nop
-	not r2
+	not r02
+	jmp HOLA
+HOLA:
 section variables
 	var1 1
 	var2 1

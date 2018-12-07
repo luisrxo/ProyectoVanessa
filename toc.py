f8be32baebe862544131ce7b82295c55f95eb5c3
luisrxo <luis.roberto.diaz.nacar@gmail.com>
2018-12-07 01:10:18 -0600

    Update toc.py

--- toc.py
+++ toc.py
@@ -1,8 +1,9 @@
+from crearC import CrearC
 class Toc:
 
 	codigoC=[]
-	
-	def analizaTodo(self, lines):
+	writ=CrearC()
+	def analizaTodo(self, lines,ruta):
 		con=0	
 		nueva=""
 		index=0
@@ -37,28 +38,28 @@ class Toc:
 		while cadena < len(nueva):
 			#MOV
 			if(nueva[index:index2]=='0010'):
-				aux="r"+str(contr)+"="+nueva[index+8:index2+8]
+				aux="r"+str(contr)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(aux)
 				contr=contr+1
 				index=index+12
 				index2=index2+12
 				cadena=cadena+12
 			elif(nueva[index:index2]=='0011'):
-				aux="var"+str(contv)+"="+nueva[index+8:index2+8]
+				aux="var"+str(contv)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(aux)
 				contv=contv+1
 				index=index+12
 				index2=index2+12
 				cadena=cadena+12
 			elif(nueva[index:index2]=='0012'):
-				aux="r"+str(contr)+"="+nueva[index+8:index2+8]
+				aux="r"+str(contr)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(aux)
 				contr=contr+1
 				index=index+12
 				index2=index2+12
 				cadena=cadena+12
 			elif(nueva[index:index2]=='0013'):
-				aux="var"+str(contv)+"="+nueva[index+8:index2+8]
+				aux="var"+str(contv)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(aux)
 				contv=contv+1
 				index=index+12
@@ -78,14 +79,14 @@ class Toc:
 				cadena=cadena+8
 			#ADD
 			elif(nueva[index:index2]=='0016' or nueva[index:index2]=='0017' or nueva[index:index2]=='0018' or nueva[index:index2]=='0019' ):
-				op1="a"+str(conts)+"="+nueva[index+4:index2+4]
+				op1="a"+str(conts)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(op1)
-				op2="b"+str(conts)+"="+nueva[index+8:index2+8]
+				op2="b"+str(conts)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(op2)
-				other="sum"+str(conts)+"= a"+str(conts)+"+ b"+str(conts)
+				other="sum"+str(conts)+"= a"+str(conts)+"+ b"+str(conts)+";"
 				self.codigoC.append(other)
 				auxsuma=int(nueva[index+8:index2+8],16)+int(nueva[index+4:index2+4],16)
-				aux="sum"+str(conts)+"="+str(auxsuma)
+				aux="sum"+str(conts)+"="+str(auxsuma)+";"
 				self.codigoC.append(aux)
 				conts=conts+1
 				index=index+12
@@ -94,7 +95,7 @@ class Toc:
 			#DEC
 			elif(nueva[index:index2]=='001A'):
 				dec=int(nueva[index+4:index2+4],16)-1
-				aux="dec"+str(contdec)+"="+str(dec)
+				aux="dec"+str(contdec)+"="+str(dec)+";"
 				self.codigoC.append(aux)
 				contdec=contdec+1
 				index=index+8
@@ -102,14 +103,14 @@ class Toc:
 				cadena=cadena+8
 			#SUB
 			elif(nueva[index:index2]=='001B' or nueva[index:index2]=='001C' or nueva[index:index2]=='001D' or nueva[index:index2]=='001E'):
-				op1="c"+str(contresta)+"="+nueva[index+4:index2+4]
+				op1="c"+str(contresta)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(op1)
-				op2="d"+str(contresta)+"="+nueva[index+8:index2+8]
+				op2="d"+str(contresta)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(op2)
-				other="resta"+str(contresta)+"= d"+str(contresta)+"- c"+str(contresta)
+				other="resta"+str(contresta)+"= d"+str(contresta)+"- c"+str(contresta)+";"
 				self.codigoC.append(other)
 				auxresta=int(nueva[index+8:index2+8],16)-int(nueva[index+4:index2+4],16)
-				aux="rest"+str(contresta)+"="+str(auxresta)
+				aux="rest"+str(contresta)+"="+str(auxresta)+";"
 				self.codigoC.append(aux)
 				contresta=contresta+1
 				index=index+12
@@ -117,14 +118,14 @@ class Toc:
 				cadena=cadena+12
 			#MUL
 			elif(nueva[index:index2]=='001F' or nueva[index:index2]=='0020' or nueva[index:index2]=='0021' or nueva[index:index2]=='0022'):
-				op1="e"+str(contmult)+"="+nueva[index+4:index2+4]
+				op1="e"+str(contmult)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(op1)
-				op2="f"+str(contmult)+"="+nueva[index+8:index2+8]
+				op2="f"+str(contmult)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(op2)
-				other="mult"+str(contmult)+"= e"+str(contmult)+"* f"+str(contmult)
+				other="mult"+str(contmult)+"= e"+str(contmult)+"* f"+str(contmult)+";"
 				self.codigoC.append(other)
 				auxmult=int(nueva[index+8:index2+8],16)*int(nueva[index+4:index2+4],16)
-				aux="mult"+str(contmult)+"="+str(auxmult)
+				aux="mult"+str(contmult)+"="+str(auxmult)+";"
 				self.codigoC.append(aux)
 				contmult=contmult+1
 				index=index+12
@@ -132,15 +133,15 @@ class Toc:
 				cadena=cadena+12
 			#DIV
 			elif(nueva[index:index2]=='0023' or nueva[index:index2]=='0024' or nueva[index:index2]=='0025' or nueva[index:index2]=='0026'):
-				op1="g"+str(contdiv)+"="+nueva[index+4:index2+4]
+				op1="g"+str(contdiv)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(op1)
-				op2="h"+str(contdiv)+"="+nueva[index+8:index2+8]
+				op2="h"+str(contdiv)+"="+nueva[index+8:index2+8]+";"
 				self.codigoC.append(op2)
-				other="div"+str(contdiv)+"= g"+str(contdiv)+"/ h"+str(contdiv)
+				other="div"+str(contdiv)+"= g"+str(contdiv)+"/ h"+str(contdiv)+";"
 				self.codigoC.append(other)
 				try: 
 					auxdiv=int(nueva[index+8:index2+8],16)/int(nueva[index+4:index2+4],16)
-					aux="div"+str(contdiv)+"="+str(auxdiv)
+					aux="div"+str(contdiv)+"="+str(auxdiv)+";"
 					self.codigoC.append(aux)
 					contdiv=contdiv+1
 				except ZeroDivisionError:
@@ -151,7 +152,7 @@ class Toc:
 			#INC
 			elif(nueva[index:index2]=='0027'):	
 				inc=int(nueva[index+4:index2+4],16)+1
-				aux="inc"+str(continc)+"="+str(inc)
+				aux="inc"+str(continc)+"="+str(inc)+";"
 				self.codigoC.append(aux)
 				continc=continc+1
 				index=index+8
@@ -160,7 +161,7 @@ class Toc:
 			#CMP
 			elif(nueva[index:index2]=='0028'):
 				auxcmp=int(nueva[index+8:index2+8],16)-int(nueva[index+4:index2+4],16)
-				aux="cmp"+str(contcmp)+"="+str(auxcmp)
+				aux="cmp"+str(contcmp)+"="+str(auxcmp)+";"
 				self.codigoC.append(aux)
 				contcmp=contcmp+1
 				index=index+12
@@ -173,7 +174,7 @@ class Toc:
 				cadena=cadena+12
 			#printN
 			elif(nueva[index:index2]=='0036' or nueva[index:index2]=='0037'):
-				impN='printf("'+nueva[index+4:index2+4]+'")"'
+				impN='printf("'+nueva[index+4:index2+4]+'");'
 				self.codigoC.append(impN)
 				index=index+8
 				index2=index2+8
@@ -181,23 +182,23 @@ class Toc:
 			#printCh
 			elif(nueva[index:index2]=='0038' or nueva[index:index2]=='0039'):
 
-				impCh='printf("'+nueva[index+4:index2+4]+'")'
+				impCh='printf("'+nueva[index+4:index2+4]+'");'
 				self.codigoC.append(impCh)
 				index=index+8
 				index2=index2+8
 				cadena=cadena+8
 			#printS
 			elif(nueva[index:index2]=='003A'):
-				impS='printf("'+nueva[index+4:index2+4]+'")'
+				impS='printf("'+nueva[index+4:index2+4]+'");'
 				self.codigoC.append(impS)
 				index=index+12
 				index2=index2+12
 				cadena=cadena+12
 			#readN
 			elif(nueva[index:index2]=='003B'):
-				aux="cst"+str(contP)+"="+nueva[index+4:index2+4]
+				aux="cst"+str(contP)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(aux)
-				leeN='scanf("%d",&cst'+str(contP)+')'
+				leeN='scanf("%d",&cst'+str(contP)+');'
 				self.codigoC.append(leeN)
 				contP=contP+1
 				index=index+8
@@ -205,18 +206,18 @@ class Toc:
 				cadena=cadena+8
 			#readCh
 			elif(nueva[index:index2]=='003C'):
-				aux="cst"+str(contP)+"="+nueva[index+4:index2+4]
+				aux="cst"+str(contP)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(aux)
-				leeN='scanf("%c",&cst'+str(contP)+')'
+				leeN='scanf("%c",&cst'+str(contP)+');'
 				self.codigoC.append(leeN)
 				index=index+8
 				index2=index2+8
 				cadena=cadena+8
 			#readS
 			elif(nueva[index:index2]=='003D'):
-				aux="cst"+str(contP)+"="+nueva[index+4:index2+4]
+				aux="cst"+str(contP)+"="+nueva[index+4:index2+4]+";"
 				self.codigoC.append(aux)
-				leeS='scanf("%s",&cst'+str(contP)+')'
+				leeS='scanf("%s",&cst'+str(contP)+');'
 				self.codigoC.append(leeS)
 				index=index+12
 				index2=index2+12
@@ -293,7 +294,16 @@ class Toc:
 				cadena=cadena+4
 	
 		print(self.codigoC)
+		self.toArch(self.codigoC,ruta)	
+	def toArch(self,codigoC,ruta):
+		codigoenc=""
+		codigoenc=codigoenc+"#include<stdio.h>"+"\n"
+		codigoenc=codigoenc+"#include<stdlib.h>"+"\n"
+		codigoenc=codigoenc+"#include<dos.h>"+"\n"
+		codigoenc=codigoenc+"#include<windows.h>"+"\n"
+		codigoenc=codigoenc+"int main() {"
+		for paso in codigoC:
+			codigoenc=codigoenc+paso+'\n'
+		codigoenc=codigoenc+'}'
+		self.writ.Crear(ruta,codigoenc)
 		
-
-  
-
